8793da54c9fe5e5d15abe095c6f7f25b762f027a
MariaShebeko <marygro89@gmail.com>
2021-11-06 12:20:00 +0200

    setup common styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './sass/main.scss';
+// import ApiService from './js/apiService';
+
+// const moviesAPIService = new ApiService();
+// console.log(moviesAPIService.fetchPopularMovies());
---
+++ src/js/apiService.js
@@ -0,0 +1,42 @@
+const API_KEY = 'd9be23358e97f87c33dbf928d8eaec37';
+const BASE_URL = `https://api.themoviedb.org/3`;
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchPopularMovies() {
+    const url = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${this.page}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ results }) => {
+        this.incrementPage();
+        return results;
+      });
+  }
+  fetchSearchMovies() {
+    const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ results }) => {
+        this.incrementPage();
+        return results;
+      });
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}

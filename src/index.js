02e7acddeec3b474f3c1a87ac6a9284eaebb0ee7
Serhii Kondratiuk <sergey932015@gmail.com>
2021-11-08 15:51:40 +0200

    footer/feature

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './js/markup-trending.js';
 
 import './js/refs/refs.js';
 import './js/main.js';
-
+import './js/modal-team.js';
 import './js/components/buttonUp.js';
 
+import 'animate.css';

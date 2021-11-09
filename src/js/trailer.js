8c91bcd4f3795a5f6d8ec583a20326b500677f02
Fosa1990 <fosa1990@gmail.com>
2021-11-09 07:41:59 +0200

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -21,12 +21,14 @@ async function onDrawModalFromTrailer(id) {
       };
       const instance = basicLightbox.create(`
   <h2 class="trailer-title">${responseData.name}</h2>
-  <iframe width="560" height="315"
+  <div class="trailer-container">
+  <iframe class="responsive-iframe" width="560" height="315"
   src='https://www.youtube.com/embed/${responseData.id}'
   frameborder="0"
   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
   allowfullscreen>
-  </iframe>`);
+  </iframe>
+  </div>`);
 
       instance.show();
     });
@@ -34,13 +36,15 @@ async function onDrawModalFromTrailer(id) {
     console.log('onDrawModalFromTrailer__catch-error: ', error);
 
     const instance = basicLightbox.create(`
-    <h2 class="trailer-title">404 video trailer not found</h2>
-    <iframe width="560" height="315"
-    src='http://www.youtube.com/embed/zwBpUdZ0lrQ'
-    frameborder="0"
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen>
-    </iframe>`);
+  <h2 class="trailer-title">404 video trailer not found</h2>
+  <div class="trailer-container">
+  <iframe class="responsive-iframe" width="560" height="315"
+  src='http://www.youtube.com/embed/zwBpUdZ0lrQ'
+  frameborder="0"
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen>
+  </iframe>
+  </div>`);
 
     instance.show();
   }

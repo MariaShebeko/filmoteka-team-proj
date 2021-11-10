8c8d0574b5df0b3d3ed05d47086b050a351aa7cd
Fosa1990 <fosa1990@gmail.com>
2021-11-10 21:42:20 +0200

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -30,7 +30,7 @@ async function onDrawModalFromTrailer(id) {
   height="315"
   src='https://www.youtube.com/embed/${responseData.id}'
   frameborder="0"
-  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
   allowfullscreen>
   </iframe>
 
@@ -45,13 +45,15 @@ async function onDrawModalFromTrailer(id) {
 
   <div class="trailer-container">
 
-<iframe class="responsive-iframe"
+<iframe
+class="responsive-iframe"
 width="560"
 height="315"
 src="https://www.youtube.com/embed/2U76x2fD_tE"
 title="YouTube video player"
 frameborder="0"
-allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+allowfullscreen>
 </iframe>
 
   </div>`);

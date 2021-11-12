53523208c2fcbd93a51100ab8e164dcada08f5ff
Fosa1990 <fosa1990@gmail.com>
2021-11-12 09:16:22 +0200

    Update trailer.js

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -6,15 +6,6 @@ import refs from './refs/refs.js';
 const { bodyEl } = refs;
 const videoAPI = new API();
 
-function onPressedEscapeCloseTrailer(event) {
-  if (!document.querySelector('.basicLightbox')) return;
-  if (event.key === 'Escape' || event.code === 'Escape' || event.keyCode == 27) {
-    const trailerBackdropEl = document.querySelector('.basicLightbox');
-    trailerBackdropEl.classList.remove('basicLightbox--visible');
-    bodyEl.removeEventListener('keydown', onPressedEscapeCloseTrailer);
-  }
-}
-
 function onCreateTrailerLink(elementsRef) {
   elementsRef.forEach(element => {
     element.addEventListener('click', e => {
@@ -39,6 +30,15 @@ async function onDrawModalFromTrailer(id) {
     const instance = basicLightbox.create(trailerVideoErrorTemplate());
     instance.show();
     bodyEl.addEventListener('keydown', onPressedEscapeCloseTrailer);
+  }
+}
+
+function onPressedEscapeCloseTrailer(event) {
+  if (!document.querySelector('.basicLightbox')) return;
+  if (event.key === 'Escape' || event.code === 'Escape' || event.keyCode == 27) {
+    const trailerBackdropEl = document.querySelector('.basicLightbox');
+    trailerBackdropEl.classList.remove('basicLightbox--visible');
+    bodyEl.removeEventListener('keydown', onPressedEscapeCloseTrailer);
   }
 }
 

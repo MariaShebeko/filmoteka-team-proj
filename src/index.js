0003a3f66a6accb848c23c6f522d7a3bbf2ea31d
MariaShebeko <marygro89@gmail.com>
2021-11-16 13:32:38 +0200

    second rename

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './sass/main.scss';
 
 import './js/components/pagination.js';
-import './js/apiService.js';
+import './js/api-service.js';
 
-import './js/markupHome.js';
+import './js/markup-home.js';
 import './js/markup-my-library.js';
 
 import './js/refs/refs.js';
 
-import './js/modalTeam.js';
+import './js/modal-team.js';
 import './js/components/button-up.js';
 
 import './js/layout/slider.js';
@@ -16,9 +16,9 @@ import './sass/components/basicLightbox.min.css';
 import './js/components/basicLightbox.min.js';
 import './js/trailer.js';
 
-import './js/searchMovie';
+import './js/search-movie.js';
 
-import './js/theme-switch';
+import './js/theme-switch.js';
 
 import 'animate.css';
 
@@ -27,4 +27,4 @@ import './js/add-to-library.js';
 import './js/loader.js';
 import './js/filter.js';
 
-import './js/components/animation';
+import './js/components/animation.js';
